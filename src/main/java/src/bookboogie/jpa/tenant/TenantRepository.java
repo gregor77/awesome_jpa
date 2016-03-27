cda0fee29fbea902c3ffc01d89088d6eead35722
dwayne park <gregor77@naver.com>
2016-03-28 00:48:34 +0900

    add QueryDSL QEntity class

--- src/main/java/src/bookboogie/jpa/tenant/TenantRepository.java
+++ src/main/java/src/bookboogie/jpa/tenant/TenantRepository.java
@@ -1,9 +1,10 @@
 package src.bookboogie.jpa.tenant;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TenantRepository extends JpaRepository<Tenant, String>{
+public interface TenantRepository extends JpaRepository<Tenant, String>, QueryDslPredicateExecutor<Tenant>{
     Tenant findByTenantId(Long tenantId);
 }
